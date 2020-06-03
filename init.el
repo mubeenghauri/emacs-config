b29d0ddc50f0fef22440bb9463d3647d6d09f1b3
mubeenghauri <p176107@nu.edu.pk>
2020-06-03 12:36:51 +0500

    Changed font to Monospace.

--- init.el
+++ init.el
@@ -144,8 +144,8 @@ values."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Source Code Pro"
-                               :size 16
+   dotspacemacs-default-font '("Monospace"                          ;; changed from "Source Code Pro"
+                               :size 18
                                :weight normal
                                :width normal
                                :powerline-scale 1.3)
